2d070d916be511329c29be0395d298d33645ffbb
kmly <kemilyteixeira047@gmail.com>
2022-08-02 22:14:02 -0300

    primeiro commit

--- src/ExercicioUm.java
+++ src/ExercicioUm.java
@@ -31,31 +31,75 @@ public class ExercicioUm {
         switch (menu) {
             // 1) Crie um programa que calcule a média (`(nota1 + nota2 + nota3 / 3)`)
             case 1:
-                // media(double notaUm, double notaDois, double notaTres)
+                
+                double notaUm = 1.5;
+                double notaDois = 5.0;
+                double notaTres = 10;
+
+                double media = media(notaUm, notaDois, notaTres);
+
+                System.out.println(media);
+
                 break;
             // 2) Crie um programa que calcule a área (`lado1 * lado2`)
             case 2:
-                // area(double ladoUm, double ladoDois)
+                double ladoUm = 5;
+                double ladoDois = 2;
+
+                double area = area(ladoUm, ladoDois);
+
+                System.out.println(area);
+                
                 break;
             // 3) Crie um programa que calcule o volume (`largura * altura * profundidade`)
             case 3:
-                // volume(double largura, double altura, double profundidade)
+                double largura = 10;
+                double altura = 5;
+                double profundidade = 3;
+
+                double volume = volume(largura, altura, profundidade);
+
+                System.out.println(volume);
+
                 break;
             // 4) Crie um programa que avalie se um valor é maior do que o dobro de outro valor
             case 4:
-                // valorMaiorQueDobro(double base, double verificar)
+                double base = 10;
+                double verificar = 35;
+                
+                boolean result = valorMaiorQueDobro(base, verificar);
+
+                System.out.println(result);
+                
                 break;
             // 5) Crie um programa que calcule _bhaskara_
             case 5:
-                // bhaskara(int a, int b, int c);
+                int a = 6;
+                int b = 2;
+                int c = 0;
+
+                System.out.println(bhaskara(a, b, c));
+
                 break;
             // 6) Crie um programa que calcule a velocidade média de uma viagem (`distancia (km) / tempo (h)`)
             case 6:
-                // mediaViagem(double distancia, double tempo)
+                double distancia = 180;
+                double tempo = 5;
+
+                double mediaViagem = mediaViagem(distancia, tempo);
+
+                System.out.println(mediaViagem);
+
                 break;
             // 7) Crie um programa que calcule o percentual de imposto pago (`faturamento \ qtd imposto`)
             case 7:
-                // percentualImposto(double faturamento, double qtdImposto)
+                double faturamento = 1000;
+                double qtdImposto = 50;
+
+                double percImposto = percentualImposto(faturamento, qtdImposto);
+
+                System.out.println(percImposto);
+
                 break;
             // 8) Crie um programa que teste se um valor é par ou ímpar
             case 8:
@@ -91,31 +135,56 @@ public class ExercicioUm {
     }
 
     public static double media(double notaUm, double notaDois, double notaTres) {
-        return 0;
+        
+        double media = (notaUm + notaDois + notaTres)/3;
+        return media;
     }
 
     public static double area(double ladoUm, double ladoDois) {
-        return 0;
+
+        double area = ladoUm*ladoDois;        
+        return area;
     }
 
     public static double volume(double largura, double altura, double profundidade) {
-        return 0;
+
+        double volume = largura*altura*profundidade;
+        return volume;
     }
 
     public static boolean valorMaiorQueDobro(double base, double verificar) {
-        return false;
+
+        double dobroBase = base*2;
+
+        if (verificar > dobroBase) 
+            return true;
+         else 
+            return false;
     }
 
     public static double[] bhaskara(int a, int b, int c) {
-        return new double[2];
+        
+        double delta = (b*b) - (4*a*c);
+        double raiz = Math.sqrt(delta);
+
+        double[] x = new double[2];
+
+        x[0] = ((b*(-1)) + raiz)/(2*a);
+        x[1] = ((b*(-1)) - raiz)/(2*a);
+
+        return x; 
     }
 
     public static double mediaViagem(double distancia, double tempo) {
-        return 0;
+        double velocidadeMedia = distancia/tempo;
+
+        return velocidadeMedia;
     }
 
     public static double percentualImposto(double faturamento, double qtdImposto) {
-        return 0;
+        double percImposto = faturamento/qtdImposto;
+
+        return percImposto;
     }
 
     public static boolean parOuImpar(int valor) {
